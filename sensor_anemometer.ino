736e82e359a8a989326cb83242e0df04f1748793
Joel Cormack <joel@robottoilets.co.uk>
2014-10-30 17:15:13 +0000

    addDataPoint() function created, printout function created

--- sensor_anemometer.ino
+++ sensor_anemometer.ino
@@ -5,8 +5,13 @@ this sketch is a skeleton of a generic sensor sketch, at the moment it is deisgn
       =====
 * genralise sketch
 * import time object
+*instead of dataPoint array, could it be a String array and we can just separate the value from timestamp with a / or something?
 */
 //create new instance of sensor with pin number and resolution
+
+#include <Time.h>  
+#include <Wire.h>  
+#include <DS1307RTC.h>  // a basic DS1307 library that returns time as a time_t
 
 #define LED 13            //define variable led to pin 13
 #define RESOLUTION 10000   //define resolution of anemometer sensor
@@ -17,43 +22,47 @@ this sketch is a skeleton of a generic sensor sketch, at the moment it is deisgn
 //STEP 1 - INITIALISATION
 void setup(){
   Serial.begin(9600);        //begin serial
+  setSyncProvider(RTC.get);   // the function to sync the time from the RTC  
   pinMode(LED, OUTPUT);      //set pin mode to output
   pinMode(HALL_EFFECT,INPUT);      //set input pin
 }
 
 void loop(){
 //STEP 2 - READ SENSORS
-  //Serial.println("HI");
- // print();
  print2();
 //STEP 3 - PROCESSING
 }
 
 //CLASS DECLARATION
-
 class Sensor {
   //private functions and variables
   private:
   byte pin;                             //the pin the sensor is plugged into, on the arduino board
   unsigned int resolution;              //the delay time between saving dataPoints
   unsigned long previousMillis;         //variable to store previousMillis for interval
+  byte arrayPosition;                  //to store array position
+  
   //public funtions and variables
   public:
+  struct dataPoint{
+    byte value;        //to hold value of sensor
+    time_t timeStamp; //standard C time_t so elapsed times can be easily calculated and time values shared across different platforms.
+  };
+  //CONSTRUCTORS
   Sensor();
   Sensor(byte, unsigned int);
+  //FUNCTIONS
   unsigned int getData();               //contains a while loop to listen for sesnor data from pin
-  void saveData();                      //store dtat point in datapoint array
   void reportData();                    //push dataPoint array to higher level
   void clearData();                     //clear dataPoint array
-
   byte getPin(void) {return pin;} 
   int getResolution(void) {return resolution;} 
+  void addDataPoint(byte);            
+  dataPoint dataPointArray[3];              //dataPoint array should contain all datapoints created in 
+  String printOutput();
+  byte printValue(int);
+  time_t printTimeStamp(int);
   
-  struct dataPoint {
-    byte value;
-    String timeStamp; //standard C time_t so elapsed times can be easily calculated and time values shared across different platforms.
-  }dataPointOne;
-  dataPoint dataPointArray[];              //dataPoint array should contain all datapoints created in 
 };//END SENSOR CLASS
 
 //DEFAULT CONSTRUCTOR
@@ -66,6 +75,7 @@ Sensor::Sensor() {
 Sensor::Sensor (byte p, unsigned int r) {
   pin = p;              //pin number 
   resolution = r;       //resolution of data
+  struct dataPoint;
 }//END CONSTRUCTOR
 
 
@@ -95,21 +105,49 @@ unsigned int Sensor::getData() {
   return count;                   //return total counts
 }//end loop
 
-void Sensor::saveData() {
-//create time object
+//this function takes in a value and creates a timestamp and adds them both to the dataPointArray
+void Sensor::addDataPoint(byte v) {
+  time_t timeNow = now();              //get the current time
+  dataPoint newDataPoint;              //create instance of structure dataPoint
+  newDataPoint.value = v;              //add value to structure
+  newDataPoint.timeStamp = timeNow;    //add the timestamp to 
+  dataPointArray[arrayPosition] = newDataPoint;
+  arrayPosition++;
 }
 void reportData(){
 }
 void clearData(){
 }
+
+byte Sensor::printValue(int i){
+  return dataPointArray[i].value;
+}
+time_t Sensor::printTimeStamp(int i){
+  return dataPointArray[i].timeStamp;
+}
+String Sensor::printOutput(){
+  for (int i = 0; i < 3; i++) {
+    Serial.print("VALUE = ");
+  Serial.println(printValue(i));
+  Serial.print("TIME STAMP = ");
+  Serial.println(printTimeStamp(i));
+  Serial.println("");
+  }
+ 
+}
 void print2() {
   Sensor anemometer (HALL_EFFECT,RESOLUTION);
-  Serial.print(anemometer.getData());
+  int data = anemometer.getData();
+  anemometer.addDataPoint(data);
+  
+  Serial.print("in the array = ");
+  anemometer.printOutput();
   Serial.print(" rotations per ");
   Serial.print(RESOLUTION);
   Serial.print(" milliseconds");
   Serial.println("");
   Serial.println("");
+  Serial.print("In the dataPoint Array");
 }
  
 //STEP 4 - OUTPUT
@@ -120,6 +158,3 @@ void blink(){
   delay(10);
   digitalWrite(LED,LOW);
 }
-
-
-
